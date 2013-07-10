9b111f7b8273e5b05a0b4f83232dab1169e71eb5
Irvin Zhan <izhan@princeton.edu>
2013-07-09 21:48:47 -0400

    started new/details of tasks.  changed angular model

--- HTML/js/ListController.js
+++ HTML/js/ListController.js
@@ -1,19 +1,40 @@
 function ListController($scope)
 {
+	// assignee must be unique user_id
 	$scope.tasks = [
 	{
-		"title": "Do the dishes"
+		"title": "Do the dishes1",
+		"details": "detail1 detail1 detail1 detail1",
+		"isCompleted": true,
+		"nagStatus": 1,
+		"assignee": "uniqueuser1"
 	},
 	{
-		"title": "Do the dishes"
+		"title": "Do the dishes2",
+		"details": "detail2 detail2 detail2 detail2",
+		"isCompleted": false,
+		"nagStatus": 2,
+		"assignee": "uniqueuser1"
 	},
 	{
-		"title": "Do the dishes"
+		"title": "Do the dishes3",
+		"details": "detail3 detail3 detail3 detail3",
+		"isCompleted": true,
+		"nagStatus": 3,
+		"assignee": "uniqueuser1"
 	},
 	{
-		"title": "Do the dishes"
+		"title": "Do the dishes4",
+		"details": "detail4 detail4 detail4 detail4",
+		"isCompleted": false,
+		"nagStatus": 4,
+		"assignee": "uniqueuser2"
 	},
 	{
-		"title": "Do the dishes"
+		"title": "Do the dishes5",
+		"details": "detail5 detail5 detail5 detail5",
+		"isCompleted": true,
+		"nagStatus": 5,
+		"assignee": "uniqueuser2"
 	}];
 }
