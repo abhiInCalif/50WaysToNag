bfdc5099c7bd58830ec2a11e48c3c773e92594d9
Irvin Zhan <izhan@princeton.edu>
2013-07-09 22:27:51 -0400

    commented out router

--- HTML/js/ListController.js
+++ HTML/js/ListController.js
@@ -3,35 +3,35 @@ function ListController($scope)
 	// assignee must be unique user_id
 	$scope.tasks = [
 	{
-		"title": "Do the dishes1",
+		"title": "AA the dishes1",
 		"details": "detail1 detail1 detail1 detail1",
 		"isCompleted": true,
 		"nagStatus": 1,
 		"assignee": "uniqueuser1"
 	},
 	{
-		"title": "Do the dishes2",
+		"title": "DD the dishes2",
 		"details": "detail2 detail2 detail2 detail2",
 		"isCompleted": false,
 		"nagStatus": 2,
 		"assignee": "uniqueuser1"
 	},
 	{
-		"title": "Do the dishes3",
+		"title": "BB the dishes3",
 		"details": "detail3 detail3 detail3 detail3",
 		"isCompleted": true,
 		"nagStatus": 3,
 		"assignee": "uniqueuser1"
 	},
 	{
-		"title": "Do the dishes4",
+		"title": "CC the dishes4",
 		"details": "detail4 detail4 detail4 detail4",
 		"isCompleted": false,
 		"nagStatus": 4,
 		"assignee": "uniqueuser2"
 	},
 	{
-		"title": "Do the dishes5",
+		"title": "EE the dishes5",
 		"details": "detail5 detail5 detail5 detail5",
 		"isCompleted": true,
 		"nagStatus": 5,
--- HTML/js/base.js
+++ HTML/js/base.js
@@ -1,1 +1,11 @@
 var app = angular.module("bday", []);
+
+/*
+angular.module('bday', []).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/list', {templateUrl: 'list.html',   controller: ListController}).
+      when('/auth', {templateUrl: 'auth.html', controller: ListController}).
+      otherwise({redirectTo: '/list'});
+}]);
+*/
