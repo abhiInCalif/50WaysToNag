afd38537bd2d93107c24bfc4b6cbd9b7586ec9ca
Irvin Zhan <izhan@princeton.edu>
2013-07-12 17:37:44 -0400

    routing works with adapter

--- HTML/js/base.js
+++ HTML/js/base.js
@@ -1,7 +1,7 @@
 var app = angular.module('bday', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/temp', {templateUrl: 'temp.html'}).
-      when('/auth', {templateUrl: 'auth.html'}).
-      otherwise({redirectTo: '/index.html'});
+      when('/', {templateUrl: 'index.html'}).
+      when('/list', {templateUrl: 'list.html'}).
+      otherwise({redirectTo: '/'});
 }]);
