2a00dd57c582aa9fa01c6f33bc00b6e98536d152
Irvin Zhan <izhan@princeton.edu>
2013-07-12 21:57:46 -0400

    added injection.  editing task works now

--- HTML/js/ListController.js
+++ HTML/js/ListController.js
@@ -1,45 +1,24 @@
-function ListController($scope)
+function ListController($scope, $location, $routeParams, sharedTaskList)
 {
-	// assignee must be unique user_id
-	$scope.tasks = [
-	{
-		"task_id": 1,
-		"title": "AA the dishes1",
-		"details": "detail1 detail1 detail1 detail1",
-		"isCompleted": true,
-		"nagStatus": 1,
-		"assignee": "uniqueuser1"
-	},
-	{
-		"task_id": 2,
-		"title": "DD the dishes2",
-		"details": "detail2 detail2 detail2 detail2",
-		"isCompleted": false,
-		"nagStatus": 2,
-		"assignee": "uniqueuser1"
-	},
-	{
-		"task_id": 3,
-		"title": "BB the dishes3",
-		"details": "detail3 detail3 detail3 detail3",
-		"isCompleted": true,
-		"nagStatus": 3,
-		"assignee": "uniqueuser1"
-	},
-	{
-		"task_id": 4,
-		"title": "CC the dishes4",
-		"details": "detail4 detail4 detail4 detail4",
-		"isCompleted": false,
-		"nagStatus": 4,
-		"assignee": "uniqueuser2"
-	},
-	{
-		"task_id": 5,
-		"title": "EE the dishes5",
-		"details": "detail5 detail5 detail5 detail5",
-		"isCompleted": true,
-		"nagStatus": 5,
-		"assignee": "uniqueuser2"
-	}];
+	$scope.tasks = sharedTaskList.getList();
+
+  $scope.updateTask = function() {
+    sharedTaskList.setList($scope.tasks);
+    $location.path('/list');
+  };
+
+  // O(n) for now
+  function findIndex(id)
+  {
+    for (var k = 0; k < $scope.tasks.length; k++)
+    {
+      if ($scope.tasks[k].task_id == id)
+      {
+
+        return k;
+      }
+    }
+    return false;
+  }
+  $scope.currentTask = $scope.tasks[findIndex($routeParams.taskId)];
 }
--- HTML/js/base.js
+++ HTML/js/base.js
@@ -2,6 +2,62 @@ var app = angular.module('bday', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/', {templateUrl: 'index.html'}).
-      when('/list', {templateUrl: 'list.html'}).
+      when('/list', {templateUrl: 'list.html', reloadOnSearch: false}).
+      when('/edit/:taskId', {templateUrl: 'taskDetails.html', reloadOnSearch: false}).
       otherwise({redirectTo: '/'});
 }]);
+
+// assignee must be unique user_id
+// injection to controller
+app.service('sharedTaskList', function () {
+  var taskList = [
+    {
+      "task_id": 1,
+      "title": "AA the dishes1",
+      "details": "detail1 detail1 detail1 detail1",
+      "isCompleted": true,
+      "nagStatus": 1,
+      "assignee": "uniqueuser1"
+    },
+    {
+      "task_id": 2,
+      "title": "DD the dishes2",
+      "details": "detail2 detail2 detail2 detail2",
+      "isCompleted": false,
+      "nagStatus": 2,
+      "assignee": "uniqueuser1"
+    },
+    {
+      "task_id": 3,
+      "title": "BB the dishes3",
+      "details": "detail3 detail3 detail3 detail3",
+      "isCompleted": true,
+      "nagStatus": 3,
+      "assignee": "uniqueuser1"
+    },
+    {
+      "task_id": 4,
+      "title": "CC the dishes4",
+      "details": "detail4 detail4 detail4 detail4",
+      "isCompleted": false,
+      "nagStatus": 4,
+      "assignee": "uniqueuser2"
+    },
+    {
+      "task_id": 5,
+      "title": "EE the dishes5",
+      "details": "detail5 detail5 detail5 detail5",
+      "isCompleted": true,
+      "nagStatus": 5,
+      "assignee": "uniqueuser2"
+    }];
+
+  return {
+      getList: function () {
+          return taskList;
+      },
+      setList: function(list) {
+          taskList = list;
+      }
+  };
+});
