7d8715800620a6a70107fc784c69d92cf0620790
Irvin Zhan <izhan@princeton.edu>
2013-07-14 19:53:05 -0400

    moved html to partials folder

--- HTML/js/base.js
+++ HTML/js/base.js
@@ -2,9 +2,8 @@ var app = angular.module('bday', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/', {templateUrl: 'index.html'}).
-      when('/userList', {templateUrl: 'userList.html'}).
-      when('/familyList', {templateUrl: 'familyList.html'}).
-      when('/edit/:taskId', {templateUrl: 'taskDetails.html'}).
-      when('/new', {templateUrl: 'newTask.html'}).
-      otherwise({redirectTo: '/'});
+      when('/userList', {templateUrl: 'partials/userList.html'}).
+      when('/familyList', {templateUrl: 'partials/familyList.html'}).
+      when('/edit/:taskId', {templateUrl: 'partials/taskDetails.html'}).
+      when('/new', {templateUrl: 'partials/newTask.html'});
 }]);
